30f0954f83d4bb7421d53b238633689aa7ea952e
gabcytn <cayetanogabriel03@gmail.com>
2024-12-09 20:12:29 +0800

    Received props of users to display

--- http-basic-auth-react/src/pages/App.tsx
+++ http-basic-auth-react/src/pages/App.tsx
@@ -1,7 +1,25 @@
-function App() {
+type User = {
+  id: number;
+  username: string;
+  password: string;
+  role: string;
+}
+type AppProps = {
+  users: User[]
+}
+function App({ users }: AppProps) {
   return (
     <div>
       <p>Hello, react!</p>
+      {users.map((user: User) => {
+        return (
+          <div className="card" key={user.id}>
+            <p>{user.username}</p>
+            <p>{user.password}</p>
+            <p>{user.role}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
