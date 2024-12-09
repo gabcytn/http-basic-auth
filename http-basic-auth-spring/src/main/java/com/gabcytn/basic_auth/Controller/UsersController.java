e6c916d30522d19921bc46af695297eddf5f7ec0
gabcytn <cayetanogabriel03@gmail.com>
2024-12-09 12:15:10 +0800

    Inject the user service class dependency on instance creation

--- http-basic-auth-spring/src/main/java/com/gabcytn/basic_auth/Controller/UsersController.java
+++ http-basic-auth-spring/src/main/java/com/gabcytn/basic_auth/Controller/UsersController.java
@@ -1,22 +1,32 @@
 package com.gabcytn.basic_auth.Controller;
 
 import com.gabcytn.basic_auth.Model.User;
-import com.gabcytn.basic_auth.Repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.gabcytn.basic_auth.Service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsersController {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UsersController (UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UsersController (UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/users")
     public List<User> getAllUsers () {
-        return userRepository.findAll();
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> getUserById (@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/register")
+    public Optional<User> createUser (@RequestBody User user) {
+        return userService.createUser(user);
     }
 }
