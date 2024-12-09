13fc1bd78941d78f3807cb33f1747da94cd0ca8a
gabcytn <cayetanogabriel03@gmail.com>
2024-12-09 22:04:06 +0800

    Created a schema.sql for easier migration

---
+++ http-basic-auth-spring/src/main/java/com/gabcytn/basic_auth/Repository/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE http_basic_auth;
+
+CREATE TABLE users (
+   id INT PRIMARY KEY AUTO_INCREMENT,
+   username VARCHAR(100) UNIQUE NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   role VARCHAR(50)
+);
