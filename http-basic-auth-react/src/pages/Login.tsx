98d72d3107fa849ff1719a100b2de863719474dd
gabcytn <cayetanogabriel03@gmail.com>
2024-12-09 13:26:05 +0800

    Used the AuthLayout and some TextInputs for the auth pages

--- http-basic-auth-react/src/pages/Login.tsx
+++ http-basic-auth-react/src/pages/Login.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+import AuthLayout from "../layout/AuthLayout"
+import Button from "../components/Button"
+import TextInput from "../components/TextInput"
+
 function Login() {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
   return (
-    <div>Login</div>
+    <AuthLayout title="Login Page">
+      <TextInput value={username} handleType={setUsername} type="text" id="username" title="Username" />
+      <TextInput value={password} handleType={setPassword} type="password" id="password" title="Password" />
+      <Button type="submit" text="Login" />
+    </AuthLayout>
   )
 }
 
--- http-basic-auth-react/src/pages/Register.tsx
+++ http-basic-auth-react/src/pages/Register.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState } from "react";
+import AuthLayout from "../layout/AuthLayout"
+import Button from "../components/Button"
+import TextInput from "../components/TextInput"
 
 function Register() {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [confirmPassword, setConfirmPassword] = useState<string>("")
   return (
-    <div>Register</div>
+    <AuthLayout title="Registration Page">
+      <TextInput value={username} handleType={setUsername} type="text" id="username" title="Username" />
+      <TextInput value={password} handleType={setPassword} type="password" id="password" title="Password" />
+      <TextInput value={confirmPassword} handleType={setConfirmPassword} type="password" id="confirm-password" title="Confirm password" />
+      <Button type="submit" text="Register" />
+    </AuthLayout>
   )
 }
 
