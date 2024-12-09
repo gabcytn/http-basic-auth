518fe348eb21983e684a8a43e820d25d554eb5da
gabcytn <cayetanogabriel03@gmail.com>
2024-12-09 18:52:48 +0800

    Moved form tags outside the AuthLayout and into the auth pages

--- http-basic-auth-react/src/layout/AuthLayout.tsx
+++ http-basic-auth-react/src/layout/AuthLayout.tsx
@@ -5,19 +5,13 @@ type AuthLayoutProps = {
   title: string;
 }
 
-function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-  e.preventDefault();
-}
-
 function AuthLayout({ title, children }: AuthLayoutProps) {
   return (
     <div className="container">
       <div className="row">
         <div className="col-12">
           <h2 className="text-center">{title}</h2>
-          <form onSubmit={(e) => { handleSubmit(e) }} className="d-flex justify-content-center flex-column">
-            {children}
-          </form>
+          {children}
         </div>
       </div>
     </div>
