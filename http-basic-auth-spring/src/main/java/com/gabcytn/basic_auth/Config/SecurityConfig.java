92e5983d94f4e2b8c68d407fc1a93bfb45fddf39
gabcytn <cayetanogabriel03@gmail.com>
2024-12-09 22:04:37 +0800

    Refactored sessions to be stateless

--- http-basic-auth-spring/src/main/java/com/gabcytn/basic_auth/Config/SecurityConfig.java
+++ http-basic-auth-spring/src/main/java/com/gabcytn/basic_auth/Config/SecurityConfig.java
@@ -7,10 +7,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.*;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -75,6 +73,15 @@ public class SecurityConfig {
         };
 
         httpSecurity.logout(logoutConfigurerCustomizer);
+
+        Customizer<SessionManagementConfigurer<HttpSecurity>> sessionManagementConfigurerCustomizer = new Customizer<SessionManagementConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(SessionManagementConfigurer<HttpSecurity> httpSecuritySessionManagementConfigurer) {
+                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            }
+        };
+
+        httpSecurity.sessionManagement(sessionManagementConfigurerCustomizer);
         return httpSecurity.build();
     }
 
