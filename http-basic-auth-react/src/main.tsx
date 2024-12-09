882a4ee5f30db62819627a6952a568da428a8bce
gabcytn <cayetanogabriel03@gmail.com>
2024-12-09 09:58:49 +0800

    Created routing for the three pages created

--- http-basic-auth-react/src/main.tsx
+++ http-basic-auth-react/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from './pages/App.tsx'
+import Register from './pages/Register.tsx'
+import Login from './pages/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 )
